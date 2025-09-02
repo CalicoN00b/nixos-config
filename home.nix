186ddd0be9aca2fc6b79cc9707146d979e1a8757
Ian Galaviz <brianatthehouse@gmail.com>
2025-09-02 17:39:52 -0600

    added obsidian in home.nix packages

--- home.nix
+++ home.nix
@@ -25,11 +25,10 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    # Add packages that don't require additional configuration
     firefox
     discord
+    obsidian
     kitty
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
