056e6da0ba14ee9bc3f5744debd3177e8d96b999
Ian Galaviz <brianatthehouse@gmail.com>
2025-09-18 12:08:57 -0600

    trying to use home-manager to configure ssh

--- user/git.nix
+++ user/git.nix
@@ -1,13 +1,32 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-    home.packages = [ pkgs.git ];
-    programs.git = {
-        enable = true;
-        userName = "Ian Galaviz";
-        userEmail = "brianatthehouse@gmail.com";
-        extraConfig = {
-            init.defaultBranch = "main";
+    programs = {
+        git = {
+            enable = true;
+            userName = "Ian Galaviz";
+            userEmail = "brianatthehouse@gmail.com";
+
+            signing = {
+                signByDefault = true;
+                key = "~/.ssh/id_ed25519.pub";
+            };
+
+            extraConfig = {
+                init.defaultBranch = "main";
+                gpg.format = "ssh";
+            };
+        };
+
+        ssh = {
+            enable = true;
+            addKeysToAgent = "yes";
+
+            extraConfig = ''
+                IdentityFile ~/.ssh/id_ed25519
+            '';
         };
     };
+
+    services.ssh-agent.enable = true;
 }
