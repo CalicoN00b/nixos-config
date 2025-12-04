704858411995f6a8402adacaacffac9508e1f1ec
Ian Galaviz <brianatthehouse@gmail.com>
2025-12-04 13:05:43 -0700

    updated nixOS to 25.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
     description = "Ian's Flake";
 
     inputs = {
-        nixpkgs.url = "nixpkgs/nixos-25.05";
-        home-manager.url = "github:nix-community/home-manager/release-25.05";
+        nixpkgs.url = "nixpkgs/nixos-25.11";
+        home-manager.url = "github:nix-community/home-manager/release-25.11";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
-        nixvim.url = "github:nix-community/nixvim/nixos-25.05";
+        nixvim.url = "github:nix-community/nixvim/nixos-25.11";
         nixvim.inputs.nixpkgs.follows = "nixpkgs";
     };
 
