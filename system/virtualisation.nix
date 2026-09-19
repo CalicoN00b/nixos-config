ad5641625261a28e074b471367fcf0fc868dc143
Ian Galaviz <brianatthehouse@gmail.com>
2026-09-18 19:17:08 -0600

    figured out virtualization

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,15 +1,36 @@
 { pkgs, ... }:
 
 {
-    users.users.igalaviz = {
-        extraGroups = [ "docker" ];
-    };
+    users.users.igalaviz.extraGroups = [
+        "docker" 
+        "libvirtd"
+    ];
 
     environment.systemPackages = with pkgs; [
         docker-compose
+
+        virt-manager
+        virt-viewer
+        spice
+        spice-gtk
+        spice-protocol
+        virtio-win
+        win-spice
+        adwaita-icon-theme
     ];
 
     virtualisation = {
         docker.enable = true;
+
+        libvirtd = {
+            enable = true;
+            qemu = {
+                swtpm.enable = true;
+            };
+        };
+
+        spiceUSBRedirection.enable = true;
     };
+
+    services.spice-vdagentd.enable = true;
 }
